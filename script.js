dd95f02b2891007ea94616dd72f8c05f5fc90f48
William Ruales <ruales@hotmail.co.uk>
2023-03-12 19:33:40 +0000

    game, running, playerturn refactored, commit before larger refactor

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-let testArray = []
 let domTable = document.querySelectorAll("td")
 let tArray = Array.from(domTable)
-console.log(tArray)
+let testArray = []
 
 domTable.forEach(function (elem, index) {
     elem.addEventListener("click", function (e) {
         if (elem.innerText === "") {
-            console.log("pingping", table(elem, index))
+            table(elem, index), playgame()
         }
 
     });
 
 });
-console.log(domTable)
 
 const playerFactory = (name, mark, turn) => {
     const sayHello = () => console.log('hello!');
     return { name, mark, turn };
 };
-
-
 let player1 = playerFactory('player1', "o", true)
 let player2 = playerFactory("player2", "X", false)
 
-let playerTurn = (function () {
+
+let table = (function (x, index) {
+
     let currentPlay = () => {
-        console.log("innerworks")
         if (player1.turn == true || player2.turn == false) { return player1 }
         else if (player2.turn == true || player1.turn == false) return player2
     }
-    return {
-        currentPlay
-
-    }
-})();
 
 
-
-
-let table = (function (x, index) {
-    if (playerTurn.currentPlay() == player1) {
+    if (currentPlay() == player1) {
         testArray.push(index)
         x.textContent = player1.mark
         player1.turn = false
         player2.turn = true
     }
-    else if (playerTurn.currentPlay() == player2) {
+    else if (currentPlay() == player2) {
         x.textContent = player2.mark
         player1.turn = true
         player2.turn = false
     }
 })
 
+let playgame = (function () {
+    if (testArray.length >= 3) { check(testArray) }
 
+})
 
 const checkWinner = () => {
     const winningCombinations = [
@@ -65,10 +57,19 @@ const checkWinner = () => {
         [0, 4, 8],
         [2, 4, 6]
     ]
-    for (combo of winningCombinations) {
-        if (combo = testArray) { return console.log("works") }
-        else {
+    return (x) => {
 
+        for (let code of winningCombinations) {
+
+            const hasAllElems = testArray.every(elem => code.includes(elem));
+            if (hasAllElems === true) {
+                console.log("win")
+                break
+            }
         }
+
     }
+
 }
+
+let check = checkWinner()
