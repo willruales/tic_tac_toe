ace6f02647117c3c46090995dc61159c754465a0
William Ruales <ruales@hotmail.co.uk>
2023-03-12 15:38:10 +0000

    fixed array, deviation from nested array

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let tableArray = [["", "", ""], ["", "", ""], ["", "", ""]]
+let testArray = []
 
 let domTable = document.querySelectorAll("td")
 let tArray = Array.from(domTable)
 console.log(tArray)
 
-domTable.forEach(function (elem) {
+domTable.forEach(function (elem, index) {
     let row = elem.parentElement
     let cell = elem.cellIndex
     let rownum = row.rowIndex
     //elem.innerText = "ping"
     elem.addEventListener("click", function (e) {
-        if (elem.innerText === "") { console.log("pingping", table(elem, rownum, cell)) }
+        if (elem.innerText === "") {
+            console.log("pingping", table(elem, rownum, cell, index))
+        }
 
     });
 
@@ -41,15 +44,14 @@ let playerTurn = (function () {
 
 
 
-let table = (function (x, y, z) {
+let table = (function (x, y, z, index) {
     if (playerTurn.currentPlay() == player1) {
-        tableArray[y][z] = player1.mark
+        testArray.push(index)
         x.textContent = player1.mark
         player1.turn = false
         player2.turn = true
     }
     else if (playerTurn.currentPlay() == player2) {
-        tableArray[y][z] = player2.mark
         x.textContent = player2.mark
         player1.turn = true
         player2.turn = false
@@ -58,6 +60,22 @@ let table = (function (x, y, z) {
 //table()
 
 
-function findWinner() {
-    console.log(tableArray.flat())
+
+const checkWinner = () => {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (combo of winningCombinations) {
+        if (combo = testArray) { return console.log("works") }
+        else {
+
+        }
+    }
 }
